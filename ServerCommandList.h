21ccfde7dffb334f264d814658bedf80e178a4cf
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-07-02 01:31:45 +0100

    Add files via upload

--- ServerCommandList.h
+++ ServerCommandList.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <unordered_map>
+#include <unordered_set>
+#include <thread>
+#include <threadpoolapiset.h>
+class ServerCommands {
+
+	struct CommandList {
+
+		struct CommandThreads {
+			std::unordered_map<std::string, std::thread> CommandThreads;
+		};
+
+		std::unordered_set<std::string> commands;
+		std::unordered_map<std::string, std::string> MappedCommands;
+
+
+
+		CommandList() {
+			//commands being used?
+		}
+	};
+
+
+
+
+
+
+
+
+	
+};
